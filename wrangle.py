e5576a37141ff09ab88e87fd259f72a387f25d1f
Brian Clements <brian.l.clements03@gmail.com>
2021-12-02 16:00:36 -0600

    add scaling lesson and exercises; add a zillow_project file for all work that i think will be useful for later; update functions module

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 from env import host, user, password
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
 ###################### Acquire Titanic Data ######################
 
@@ -55,8 +57,10 @@ def get_zillow_data():
     return df
 
 def remove_outliers(df, k, col_list):
-    ''' remove outliers from a list of columns in a dataframe 
-        and return that dataframe
+    ''' 
+    
+    Here, we remove outliers from a list of columns in a dataframe and return that dataframe
+    
     '''
     
     for col in col_list:
@@ -81,20 +85,45 @@ def clean_and_prep_data(df):
     
     '''
     cols = ['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt', 'taxamount']
+    # dropping nulls
     df = df.dropna()
+    #removing outliers--see the function elsewhere in this file
     df = remove_outliers(df, 1.5, cols)
+    # accessing datetime info so as to create an 'age' variable
     from datetime import date
     df.yearbuilt =  df.yearbuilt.astype(int)
     year = date.today().year
     df['age'] = year - df.yearbuilt
+    # dropping the 'yearbuilt' column now that i have the age
     df = df.drop(columns=['yearbuilt'])
+    # renaming columns for ease of reading
+    df = df.rename(columns={'bedroomcnt':'bedrooms','bathroomcnt':'bathrooms','calculatedfinishedsquarefeet':'sq_ft','taxvaluedollarcnt':'tax_value','taxamount':'tax_amount'})
     return df
 
 
-def split_zillow_data(df):
+def split_zillow(df):
     '''
     Takes in the zillow dataframe and returns train, validate, test subset dataframes
     '''
-    train, test = train_test_split(df, test_size = .2, random_state=123, stratify=df.taxvaluedollarcnt)
-    train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train.taxvaluedollarcnt)
+
+    # Test set is .2 of original dataframe
+    train, test = train_test_split(df, test_size = .2, random_state=123)#, stratify= df.tax_value)
+    # The remainder is here divided .7 to train and .3 to validate
+    train, validate = train_test_split(train, test_size=.3, random_state=123)#, stratify= train.tax_value)
     return train, validate, test
+
+
+def encode_zillow(df):
+    '''
+    This is encoding a few of me zillow columns for later modelling; it drops the original column 
+    once it has been encoded
+    
+    '''
+    cols_to_dummy = df['fips']
+    dummy_df = pd.get_dummies(cols_to_dummy, dummy_na=False, drop_first=False)
+    df = pd.concat([df, dummy_df], axis = 1)
+    df.columns = df.columns.astype(str)
+    df.rename(columns={'6037.0':'LA', '6059.0': 'Orange', '6111.0':'Ventura'}, inplace=True)
+    df = df.drop(columns='fips')
+    return df
+
